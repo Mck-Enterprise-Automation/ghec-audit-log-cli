6647ea232cf941b4e9cfb85ca51d7b240f25ea91
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2020-06-15 05:32:44 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # shell script to run ghec-audit-log-cli
 # After install of Nodejs and https://github.com/Mck-Enterprise-Automation/ghec-audit-log-cli
-node ghec-audit-log-cli
+node ghec-audit-log-cli.js "--help"
