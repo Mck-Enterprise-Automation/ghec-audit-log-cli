6e69c5a2d5438499115d66b2f8d68d6f4d7a7dcf
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2020-06-15 05:41:02 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # After install of Nodejs and https://github.com/Mck-Enterprise-Automation/ghec-audit-log-cli
 # this shell script runs in McK T&D behind firewall?
-# to obtain logs from GitHub.com
-# and froward to Splunk
+# to obtain logs from GitHub.com as described at https://help.github.com/en/enterprise/2.20/admin/installation/audit-logging
+# and forward to Splunk's https://dev.splunk.com/enterprise/docs/dataapps/httpeventcollector/
 # run ghec-audit-log-cli
+# See https://help.github.com/en/enterprise/2.20/admin/installation/audit-logging
 node ghec-audit-log-cli.js "--help"
