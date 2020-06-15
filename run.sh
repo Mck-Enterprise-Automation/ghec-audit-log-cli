d2cae8e5111418c9db49846fa3fbd7c1345f72f1
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2020-06-15 05:36:43 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-# shell script to run ghec-audit-log-cli
+# run.sh
+
 # After install of Nodejs and https://github.com/Mck-Enterprise-Automation/ghec-audit-log-cli
+# this shell script runs in McK T&D behind firewall?
+# to obtain logs from GitHub.com
+# and froward to Splunk
+# run ghec-audit-log-cli
 node ghec-audit-log-cli.js "--help"
